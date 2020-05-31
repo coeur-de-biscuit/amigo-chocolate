d1ac7f0169a55707fa2753bedec1612937750911
Salt-N-Pepacls <exemplo@exemplo.com>
2020-05-31 14:04:59 -0300

    Adicionando CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -16,6 +17,7 @@ mongoose.connect(process.env.DB_CONNECTION,
     () => console.log('Conectou meu consagrado')
 )
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
