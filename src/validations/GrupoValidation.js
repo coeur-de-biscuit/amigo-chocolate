3f07d1c201f622f6fee2df98aa5387bd3a736f5b
Salt-N-Pepacls <exemplo@exemplo.com>
2020-06-07 16:00:43 -0300

    Retiurando validation

--- src/validations/GrupoValidation.js
+++ src/validations/GrupoValidation.js
@@ -2,8 +2,6 @@ const { body, validationResult } = require('express-validator');
 const GrupoValidationRules = () => {
     return [
         body('nome').isString(),
-        body('valorMinimo').isNumeric(),
-        body('valorMaximo').isNumeric(),
         body('apelido_admin').isString(),
     ]
 }
